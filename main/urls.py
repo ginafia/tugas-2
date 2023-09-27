f6869a887139a87e25bb2ae7c795298f095b89d6
ginafia <gina.afia@ui.ac.id>
2023-09-27 10:03:44 +0700

    push setelah bonus

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id , register, login_user, logout_user
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id , register, login_user, logout_user, increase, decrease, delete
 
 
 
@@ -9,6 +9,11 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'),
+
+    path('increase/<int:id>', increase, name='increase'),
+    path('decrease/<int:id>', decrease, name='decrease'),
+    path('delete/<int:id>', delete, name='delete'),
+
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
